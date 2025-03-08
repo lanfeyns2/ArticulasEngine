6bacea1b824e1fba1f58271922ffa7f371277547
lanfeyns <CHicickenmanqw@outlook.com>
2025-03-08 01:07:20 -0500

    Initial Commit

---
+++ ArticulasEngine/src/Core/Application.cpp
@@ -0,0 +1,8 @@
+#include "Application.h"
+
+namespace ArtE
+{
+	Application::Application(const char* name)
+	{
+	}
+}
---
+++ ArticulasEngine/src/Core/Application.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace ArtE
+{
+	class Application
+	{
+	public:
+		Application(const char* name);
+
+	};
+}
+
